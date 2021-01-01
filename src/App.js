6f08ef30a75e3b8b0241f3630f79bf3e3af86c58
waleedd32 <valid0@hotmail.com>
2021-01-01 13:02:14 +0200

    explained the project and put some initial styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      TUF-2000M Registers
+      {/* This is going to be Gambit challenge */}
+      {/* task Option 2: Web or native app */}
+      {/*a graphical solution, create an app that retrieves and parses 
+      the data and presents it as is. The key point 
+      is to make use of data available in a backend, 
+      and present it in a mobile friendly way. */}
     </div>
   );
 }
